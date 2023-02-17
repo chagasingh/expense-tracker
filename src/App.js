e921c54e674bef0d2a7c873b2b802985cd353fbd
chagansingh <chagansingh9876@gmail.com>
2023-02-17 09:20:47 +0530

    login added

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import { Switch} from 'react-router-dom';
+import AuthForm from './components/AuthForm/AuthForm'
+
+function App() {
+  return (
+      <Switch>
+       <AuthForm/>
+      </Switch>
+  );
+}
+
+export default App;
+
